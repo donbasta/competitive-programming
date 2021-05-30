a0ece089e34a95ab4353a297d8db4807c664f70f
donbasta <faddilafarras@gmail.com>
2021-05-30 20:23:19 +0700

    add generate divisors from prime factorization recursive

---
+++ templates/scratch/math/generate_divisors.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+void generateDivisors(int curIdx, int curDiv, vector<pair<int, int>>& ar, vector<int>& div) {
+    if (curIdx == ar.size()) { div.emplace_back(curDiv); return; }
+    for (int i = 0; i <= ar[curIdx].second; i++) {
+        generateDivisors(curIdx + 1, curDiv, ar, div);
+        curDiv *= ar[curIdx].first;
+    }
+}
+
+void solve() {
+    vector<pair<int, int>> p = {{2,3},{3,2},{7,1}};
+    vector<int> d;
+    generateDivisors(0, 1, p, d);
+    for (auto& e : d) cout << e << ' ';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    solve();
+  
+    return 0;
+}
