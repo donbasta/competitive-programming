1ca053fdea6bad9f68d06deda4e1d97352a92030
donbasta <faddilafarras@gmail.com>
2021-06-13 14:42:41 +0700

    update fact comb matrix template

--- templates/scratch/math/matrix-template.cpp
+++ templates/scratch/math/matrix-template.cpp
@@ -1,30 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+using ll = long long;
+using matrix = vector<vector<ll>>;
 
-typedef vector<vector<ll>> matrix;
 matrix identity;
 
-matrix mul(matrix a, matrix b, int sz){
+matrix mul(matrix a, matrix b){
+	int sz = (int) a.size();
 	matrix d(sz, vector<ll>(sz));
-  for(int i = 0; i < sz; i++) {
-  	for(int j = 0; j < sz; j++) {
-  		for(int k = 0; k < sz; k++) {
-  			d[i][j] = (d[i][j] + a[i][k] * b[k][j]);
-  		}
-  	}
-  }
-  return d;
+	for(int i = 0; i < sz; i++) {
+		for(int j = 0; j < sz; j++) {
+			for(int k = 0; k < sz; k++) {
+				d[i][j] = (d[i][j] + a[i][k] * b[k][j]);
+			}
+		}
+	}
+	return d;
 }
  
-matrix fast_expo(matrix a, ll b){
-	int sz = (int) a.size();
-	if(b == 0) return identity; //define identity first
-	if(b == 1) return a;
-	if(b % 2 == 1) return mul(a, fast_expo(a, b - 1), sz);
-	matrix c = fast_expo(a, b/2);
-	return mul(c, c, sz);
+// matrix fpow(matrix a, ll b) {
+// 	int sz = (int) a.size();
+// 	if(b == 0) return identity; //define identity first
+// 	if(b == 1) return a;
+// 	if(b % 2 == 1) return mul(a, fpow(a, b - 1), sz);
+// 	matrix c = fpow(a, b/2);
+// 	return mul(c, c, sz);
+// }
+
+matrix fpow(matrix a, ll b) {
+	matrix ret = identity;
+	while (b) {
+		if (b & 1) ret = mul(ret, a);
+		a = mul(a, a);
+		b >>= 1;
+	}
+	return ret;
 }
 
 void printMatrix(matrix a) {
@@ -36,7 +47,7 @@ void printMatrix(matrix a) {
 	}
 }
 
-int main(){
+int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
 
@@ -50,5 +61,4 @@ int main(){
 	}
 
 	printMatrix(identity);
-
 }
