f414d2bacf6d3a024462949e83218790f1f4fee2
donbasta <faddilafarras@gmail.com>
2021-06-24 23:12:07 +0700

    add trie bit

---
+++ templates/cheatsheet/data-structures/trie-bit.cpp
@@ -0,0 +1,38 @@
+const int BMAX = 20;
+struct Trie {
+    struct Node {
+        Node *child[2];
+        int cnt;
+        Node () {
+            child[0] = child[1] = NULL;
+            cnt = 0;
+        }
+    };
+
+    Node *head;
+
+    Trie () { head = new Node(); }
+
+    void insert (int val) { 
+        Node *cur = head;
+        for (int i = BMAX; i >= 0; i--) {
+            int v = (val >> i) & 1;
+            ++(cur -> cnt);
+            if (cur -> child[v] == NULL) {
+                cur -> child[v] = new Node ();
+            }
+            cur = cur -> child[v];
+        }
+        ++(cur -> cnt);
+    }
+
+    void erase (int val) {
+        Node *cur = head;
+        for (int i = BMAX; i >= 0; i--) {
+            int v = (val >> i) & 1;
+            --(cur -> cnt);
+            cur = cur -> child[v];
+        }
+        --(cur -> cnt);
+    }
+};
